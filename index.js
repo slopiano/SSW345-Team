90e1fcb5a5ca1499a7e9ddf6d64e03420761839a
slopiano <slopiano@stevens.edu>
2022-04-16 14:45:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Run dotenv
+require('dotenv').config();
+
+const {Client, Intents} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
