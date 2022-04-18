2f55126421ff33192989aac3490e3b5e85d934c8
slopiano <slopiano@stevens.edu>
2022-04-17 20:20:27 -0400

    api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Run dotenv
 require('dotenv').config();
+const axios = require('axios');
 
 const {Client, Intents} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -8,10 +9,34 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const api_key = process.env.CRYPTO_API
+
 client.on('message', msg => {
     if (msg.content === 'ping') {
       msg.reply('pong');
     }
+    let response = null;
+    new Promise(async (resolve, reject) => {
+    try {
+        response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+        headers: {
+            'X-CMC_PRO_API_KEY': api_key,
+        },
+        });
+    } catch(ex) {
+        response = null;
+        // error
+        console.log(ex);
+        msg.reply(ex);
+        reject(ex);
+    }
+    if (response) {
+        // success
+        const json = response.data;
+        console.log(json);
+        resolve(json);
+    }
+    });
 });
 
 client.login(process.env.BOT_TOKEN);
