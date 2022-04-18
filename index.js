29414532709b8545ce9e738c19f4d00bcae03b42
Sean Hicks <shicks@stevens.edu>
2022-04-17 23:53:08 -0400

    added eslint and help message response

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // Run dotenv
-require('dotenv').config();
+require("dotenv").config();
 
-const {Client, Intents} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-client.on('ready', () => {
+client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
+client.on("message", (msg) => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+client.on("message", (msg) => {
+    if (msg.content === "!help") {
+        msg.reply("!help - Displays this message\nping - pong\n");
     }
 });
 
