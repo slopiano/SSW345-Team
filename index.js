cc4a366952c0c2524f3f914b4995cbb844993e8c
slopiano <slopiano@stevens.edu>
2022-04-17 20:43:07 -0400

    eslint

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 // Run dotenv
-require('dotenv').config();
-const axios = require('axios');
+require('dotenv').config()
+const axios = require('axios')
 
-const {Client, Intents} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-const api_key = process.env.CRYPTO_API
+const apiKey = process.env.CRYPTO_API
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-    }
-    let response = null;
-    new Promise(async (resolve, reject) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong')
+  }
+  let response = null
+  new Promise(async (resolve, reject) => {
     try {
-        response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
         headers: {
-            'X-CMC_PRO_API_KEY': api_key,
-        },
-        });
-    } catch(ex) {
-        response = null;
-        // error
-        console.log(ex);
-        msg.reply(ex);
-        reject(ex);
+          'X-CMC_PRO_API_KEY': apiKey
+        }
+      })
+    } catch (ex) {
+      response = null
+      // error
+      console.log(ex)
+      msg.reply(ex)
+      reject(ex)
     }
     if (response) {
-        // success
-        const json = response.data;
-        console.log(json);
-        resolve(json);
+      // success
+      const json = response.data
+      const price = getPrice(json)
+      msg.reply(price)
+      resolve(json)
     }
-    });
-});
+  })
+})
 
-client.login(process.env.BOT_TOKEN);
+function getPrice (json) {
+
+}
+
+client.login(process.env.BOT_TOKEN)
