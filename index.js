254195713c882f1b54d8da86fc9e9fd5ca1e5c89
slopiano <slopiano@stevens.edu>
2022-04-21 19:21:06 -0400

    gets prices

--- index.js
+++ index.js
@@ -11,14 +11,40 @@ client.on('ready', () => {
 
 const apiKey = process.env.CRYPTO_API
 
+// var idMap = {}
+// let map_response = null
+//   new Promise(async (resolve, reject) => {
+//     try {
+//       map_response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+//         headers: {
+//           'X-CMC_PRO_API_KEY': apiKey
+//         }
+//       })
+//     } catch (ex) {
+//       response = null
+//       // error
+//       console.log(ex)
+//       reject(ex)
+//     }
+//     if (response) {
+//       // success
+//       const json = response.data
+//       const string = JSON.stringify(json)
+//       const data = JSON.parse(string)
+      
+//       resolve(json)
+//     }
+// })
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong')
-  }
-  let response = null
-  new Promise(async (resolve, reject) => {
+  if(msg.content.length > 6){
+  if (msg.content.slice(0,3) === 'get') {
+    const cryptSymbol = msg.content.slice(4,7)
+    let response = null
+    new Promise(async (resolve, reject) => {
     try {
-      response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest', {
+        params: { symbol: cryptSymbol},
         headers: {
           'X-CMC_PRO_API_KEY': apiKey
         }
@@ -29,51 +55,53 @@ client.on('message', msg => {
       console.log(ex)
       msg.reply(ex)
       reject(ex)
+      console.log(msg.content.slice(0,3));
+      console.log(msg.content.slice(4,7));
     }
     if (response) {
       // success
       const json = response.data
       const string = JSON.stringify(json)
       const data = JSON.parse(string)
-      const price = getPrice(data)
-      msg.reply(price)
+      const price = getPrice(data, cryptSymbol)
+      let priceString = price.toString();
+      msg.reply(priceString)
       resolve(json)
     }
   })
+  }
+}
 })
 
 
-let response = null
-  new Promise(async (resolve, reject) => {
-    try {
-      response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-        headers: {
-          'X-CMC_PRO_API_KEY': apiKey
-        }
-      })
-    } catch (ex) {
-      response = null
-      // error
-      console.log(ex)
-      msg.reply(ex)
-      reject(ex)
-    }
-    if (response) {
-      // success
-      const json = response.data
-      const string = JSON.stringify(json)
-      const data = JSON.parse(string)
-      const price = getPrice(data)
-      msg.reply(price)
-      resolve(json)
-    }
-})
+// let response = null
+//   new Promise(async (resolve, reject) => {
+//     try {
+//       response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC', {
+//         headers: {
+//           'X-CMC_PRO_API_KEY': apiKey
+//         }
+//       })
+//     } catch (ex) {
+//       response = null
+//       // error
+//       console.log(ex)
+//       reject(ex)
+//     }
+//     if (response) {
+//       // success
+//       const json = response.data
+//       const string = JSON.stringify(json)
+//       const data = JSON.parse(string)
+//       console.log(data);
+//       resolve(json)
+//     }
+// })
 
-function getPrice (json) {
-  let data = json.data[0]
-  let coin = data.symbol
-  console.log(coin);
-  return coin["name"]
+function getPrice (json, cryptSymbol) {
+  let data = json['data'][cryptSymbol]['quote']['USD']['price']
+  console.log(data);
+  return data
 }
 
 client.login(process.env.BOT_TOKEN);
